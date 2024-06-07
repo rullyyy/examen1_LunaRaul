1b9ba7920d207c01bbfc906958e82b471b4592f2
Raúl Luna <jesus.luna235765@potros.itson.edu.mx>
2024-06-07 13:53:50 -0700

    Arreglo de errores

--- examen1_235765/app/src/main/java/itson/edu/mx/examen1_235765/MainActivity.kt
+++ examen1_235765/app/src/main/java/itson/edu/mx/examen1_235765/MainActivity.kt
@@ -7,54 +7,72 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    val btn5: Button = findViewById(R.id.btn5)
-    val btn10: Button = findViewById(R.id.btn10)
-    val btn15: Button = findViewById(R.id.btn15)
-    val btn20: Button = findViewById(R.id.btn20)
-    val btn25: Button = findViewById(R.id.btn25)
-    val btn50: Button = findViewById(R.id.btn50)
-    val btnTip: Button = findViewById(R.id.btnTip)
-    val btnDiscount: Button = findViewById(R.id.btnDiscount)
-    var originalPrice: TextView = findViewById(R.id.ptOriginalPrice)
-    val txtPercentage: TextView = findViewById(R.id.percentage)
-    val txtTotal: TextView = findViewById(R.id.total)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var price: Double = originalPrice.text.toString().toDouble()
-        var percentage: Double = txtPercentage.text.toString().toDouble()
+
+        val btn5: Button = findViewById(R.id.btn5) as Button
+        val btn10: Button = findViewById(R.id.btn10) as Button
+        val btn15: Button = findViewById(R.id.btn15) as Button
+        val btn20: Button = findViewById(R.id.btn20) as Button
+        val btn25: Button = findViewById(R.id.btn25) as Button
+        val btn50: Button = findViewById(R.id.btn50) as Button
+        val btnTip: Button = findViewById(R.id.btnTip) as Button
+        val btnDiscount: Button = findViewById(R.id.btnDiscount) as Button
+        var originalPrice: TextView = findViewById(R.id.ptOriginalPrice) as TextView
+        val txtPercentage: TextView = findViewById(R.id.percentage) as TextView
+        val txtTotal: TextView = findViewById(R.id.total) as TextView
+
+        var addPercentage = 0.0
+        var quitPercentage = 0.0
+        var price = 0.0
+        var percentage = 0.0
+
+        try {
+            price = originalPrice.text.toString().toDouble()
+            percentage = txtPercentage.text.toString().toDouble()
+        } catch (e: java.lang.Exception){
+            txtPercentage.setText("Escriba un número")
+        }
 
         btn5.setOnClickListener {
-             percentage = price * (0.05)
+            price = originalPrice.text.toString().toDouble()
+            percentage = price * (0.05)
             txtPercentage.text = percentage.toString()
         }
         btn10.setOnClickListener {
+            price = originalPrice.text.toString().toDouble()
             percentage = price * (0.1)
             txtPercentage.text = percentage.toString()
         }
         btn15.setOnClickListener {
+            price = originalPrice.text.toString().toDouble()
             percentage = price * (0.15)
             txtPercentage.text = percentage.toString()
         }
         btn20.setOnClickListener {
+            price = originalPrice.text.toString().toDouble()
             percentage = price * (0.20)
             txtPercentage.text = percentage.toString()
         }
         btn25.setOnClickListener {
+            price = originalPrice.text.toString().toDouble()
             percentage = price * (0.25)
             txtPercentage.text = percentage.toString()
         }
         btn50.setOnClickListener {
+            price = originalPrice.text.toString().toDouble()
             percentage = price * (0.50)
             txtPercentage.text = percentage.toString()
         }
         btnTip.setOnClickListener {
-            var addPercentage = price + percentage
+            price = originalPrice.text.toString().toDouble()
+            addPercentage = price + percentage
             txtTotal.text = addPercentage.toString()
         }
         btnDiscount.setOnClickListener {
-            var quitPercentage = price - percentage
+            price = originalPrice.text.toString().toDouble()
+            quitPercentage = price - percentage
             txtTotal.text = quitPercentage.toString()
         }
 
